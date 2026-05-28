0b90ac370811d2bdae09fb77a5341964120b79e1
Aariz18 <mohd.aariz13518@gmail.com>
2026-05-28 17:28:00 +0530

    Making the website properly responsive, just finshed the navbar for the phone only.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,38 @@
 import Button from "./Button";
 import logo from "../assets/logo.png";
+import sidebarToggle from "../assets/three-line-horizontal.png";
 
-function Navbar(){
+interface NavbarProps{
+    setSidebarToggle:React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+function Navbar({setSidebarToggle}:NavbarProps){
     return(
-        <div className="flex flex-row shadow-navbar lg:pl-6.25 lg:py-[10px] lg:gap-[1px] w-full justify-center lg:h-[121px] h-[50px]" id="home">
-            <div className="flex flex-row lg:gap-[30px] gap-[3px] justify-self-start items-center cursor-pointer lg:w-[345.64px] pl-[5px]">
+        <div className="grid grid-cols-2 p-3 items-center content-center justify-center w-full shadow-navbar
+        md:grid-cols-[auto_1fr_auto] md:gap-9" id="home">
+            <div className="flex flex-row justify-self-start items-center cursor-pointer lg:w-[345.64px]">
                 <img src={logo} alt="Logo" className="
                 w-[30.03px] h-[25.61px]
                 lg:w-[150px] lg:h-[100px] lg:pl-[5px]"/>
                 <div className="lg:text-nav-title font-nav-title text-center font-semibold text-[16px]
                 lg:w-[196px] lg:h-[91px] w-[45px] h-[21px]">AARIZ</div>
             </div>
-            <div className="flex flex-row flex-1 justify-end gap-[6px] py-[10px] pr-[17px] lg:gap-[53px] items-center lg:py-[10px] lg:pe-[36px]">
-                <a href="#home" className="text-sm-nav-item lg:text-nav-item font-heading font-medium cursor-pointer lg:hover:border-b-3 hover:border-primary lg:transition-all lg:duration-75 lg:ease-in-out">home</a>
-                <a href="#work" className="text-sm-nav-item lg:text-nav-item font-heading font-medium cursor-pointer lg:hover:border-b-3 lg:hover:border-primary lg:transition-all lg:duration-75 lg:ease-in-out">work</a>
-                <a href="#aboutme" className="text-sm-nav-item lg:text-nav-item font-heading font-medium cursor-pointer lg:hover:border-b-3 lg:hover:border-primary lg:transition-all lg:duration-75 lg:ease-in-out">about me</a>
-                <a href="#contact" className="text-sm-nav-item lg:text-nav-item font-heading font-medium cursor-pointer lg:hover:border-b-3 lg:hover:border-primary lg:transition-all lg:duration-75 lg:ease-in-out">contact</a>
+            <div className="hidden flex-row justify-evenly gap-2
+            md:flex
+            mg:flex">
+                <a href="#home" className="text-sm-nav-item lg:text-nav-item font-heading font-medium cursor-pointer ">home</a>
+                <a href="#work" className="text-sm-nav-item lg:text-nav-item font-heading font-medium cursor-pointer ">work</a>
+                <a href="#aboutme" className="text-sm-nav-item lg:text-nav-item font-heading font-medium cursor-pointer ">about me</a>
+                <a href="#contact" className="text-sm-nav-item lg:text-nav-item font-heading font-medium cursor-pointer ">contact</a>
+            </div>
+            <div className="justify-self-end pr-2
+            flex gap-2">
                 <Button 
                     variant="primary"
                     content="Resume"
                 />
+                <img src={sidebarToggle} alt="sidebar toggle" className="md:hidden cursor-pointer" width="13px" height="13px"
+                onClick={()=>setSidebarToggle(true)}/>
             </div>
         </div>
     )
